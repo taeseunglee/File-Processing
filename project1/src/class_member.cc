528b68bd4537cf42fa2e7e3d5b2319515c633e1e
SGsupernova <dlxotmd125@gmail.com>
2016-09-23 22:04:56 +0900

    implement class Member

--- project1/src/class_member.cc
+++ project1/src/class_member.cc
@@ -1,29 +1,77 @@
 #include <iostream>
 #include <string>
-
+#include <string.h>
 
 using namespace std;
 
 class Member {
-	string	ID;
-	string	name;
-	string	phoneNumber;
-	string	address;
-	char	birthday[8];
-	string	eMail;
-	
+	private:
+		string	ID;
+		string	name;
+		string	phoneNumber;
+		string	address;
+		char	birthday[9];
+		string	eMail;
+
 	public:
-		Member();
+		Member () {};
+		Member (string pID, string pName, string pPhoneNumber, string pAddress, char pBirthday[8], string pEMail);
+		void operator = (const Member &M);
+		bool operator ==(const Member &M);
+		bool operator !=(const Member &M);
+		void print();
 };
 
-Member::Member(void)
+// the constructor of class "Member"
+Member::Member(string pID, string pName, string pPhoneNumber, string pAddress, char pBirthday[8], string pEMail)
 {
-	cout << "Object is being created" << endl;
+	ID			= pID;
+	name		= pName;
+	phoneNumber = pPhoneNumber;
+	address		= pAddress;
+	strcpy(birthday, pBirthday);
+	eMail		= pEMail;
+}
+void Member::operator= (const Member &M) {
+   ID			= M.ID;
+   name			= M.name;
+   phoneNumber	= M.phoneNumber;
+   address		= M.address;
+   eMail		= M.eMail;
+   strcpy(birthday, M.birthday);
 }
 
+bool operator ==(const Member &M) {
+	if (ID == M.ID) return true;
+	else			return false;
+}
+
+bool operator !=(const Member &M) {
+	if (ID != M.ID) return true;
+	else			return false;
+}
+
+// test member variable
+void Member::print() {
+	cout  << ID << endl;
+}
+
+
+// testing main function
 int main ()
 {
-	Member student;
+	char birthday[9] = "19950320";
+	Member me("supernova", "Taeseung", "010-4455-5887", "Seoul", birthday, "dlxotmd125@gmail.com"), you;
 
+	// test overloaded operator =
+	you = me;
+	you.print();
+	
+	if (you == me) {
+		cout << "I'm you!" << endl;
+	}
+	if (you != me) {
+		cout << "I'm not you" << endl;
+	}
 	return 0;
 }
