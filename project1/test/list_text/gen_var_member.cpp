55f37a455c6cd37213bbba569d681bc51be7f3be
SGsupernova <dlxotmd125@gmail.com>
2016-09-26 23:21:36 +0900

    finish

--- project1/test/list_text/gen_var_member.cpp
+++ project1/test/list_text/gen_var_member.cpp
@@ -13,29 +13,40 @@
 
 using namespace std;
 
-char*  generateRandomMemberID (void) {
+string*  generateRandomMemberId (int num) {
 	srand(time(NULL));
 	const char alphaNum[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	const int lenStr = rand() % 9 + 5,		// the max length of ID
-			lenAlphaNum = sizeof(alphaNum);
-	char* genStr = new char[lenStr + 1];
 
-	for (int i = 0; i < lenStr; ++i) genStr[i] = alphaNum[rand() % lenAlphaNum];
-	genStr[lenStr] = '\0';
+	string* idArr = new string [num];
 
-	return genStr;
+	for (int i = 0; i < num; i++) {
+		const int lenStr = rand() % 9 + 5,		// the max length of ID
+			  lenAlphaNum = sizeof(alphaNum);
+		char* genStr = new char[lenStr + 1];
+
+		for (int i = 0; i < lenStr; ++i) {
+			genStr[i] = alphaNum[rand() % lenAlphaNum];
+		}
+		genStr[lenStr] = '\0';
+
+		idArr[i] = string(genStr);
+
+		delete [] genStr;
+	}
+
+	return idArr;
 };
 
-string* generateRandomMemberName (int peopleNum) {
+string** generateRandomMemberName (int num) {
 	ifstream maleFirstFile,
 			 femaleFirstFile,
 			 allLastFile;
-	
+
 	// file open
 	maleFirstFile.open	("name_list/word_male_first.txt");
 	femaleFirstFile.open("name_list/word_female_first.txt");
 	allLastFile.open	("name_list/word_all_last.txt");
-	
+
 	// get first and last names.
 	string firstName[2][NUM_FIRST_NAME];
 	string lastName[NUM_LAST_NAME];
@@ -49,15 +60,21 @@ string* generateRandomMemberName (int peopleNum) {
 	}
 
 	// generate random names.
-	string* peopleName = new string[peopleNum];
+	string** peopleName = new string*[num];
+	for (int i = 0; i < num; i++) {
+		peopleName[i] = new string[2]; // 0 : last name / 1 : first name
+	}
 
 	srand(time(NULL));
-	for (int i = 0; i < peopleNum; i++) {
+	for (int i = 0; i < num; i++) {
 		int firstNameNumber = rand() % (NUM_FIRST_NAME),
 			lastNameNumber	= rand() % (NUM_LAST_NAME);
-		
-		peopleName[i] = firstName[i%2][firstNameNumber] + " " + lastName[lastNameNumber];
+
+		peopleName[i][0] = firstName[i%2][firstNameNumber];
+		peopleName[i][1] = lastName[lastNameNumber];
+//		peopleName[i] = firstName[i%2][firstNameNumber] + " " + lastName[lastNameNumber];
 	}
+
 
 	return peopleName;
 };
@@ -73,11 +90,11 @@ string* generateRandomPhoneNumber (int num) {
 		sprintf(fourNumber, "%04d", i);
 		middlePhoneNumber[i] = lastPhoneNumber[i] = string(fourNumber);
 	}
-	
+
 	string* phoneNumbers = new string [num];
-	
+
 	srand(time(NULL));
-	
+
 	for (int i = 0; i < num; i++) {
 		int frontNumber	= rand() % 11,
 			middleNumber= rand() % 10000,
@@ -92,26 +109,74 @@ string* generateRandomPhoneNumber (int num) {
 	return phoneNumbers;
 }
 
+string* generateRandomAddress() {
+	const int NUM_CITY = 135;
+	ifstream cityFile;
+	cityFile.open("cityName.txt");
+
+	string* addressList = new string[NUM_CITY];
+
+	for (int i = 0; i < NUM_CITY; i++) {
+		cityFile >> addressList[i];
+	}
+
+	return addressList;
+}
+
+string* generateRandomBirthday(int num) {
+	srand(time(NULL));
+
+	char birthday[9] = {0,};
+	string* birthdayArr = new string [num];
+
+	for (int i = 0; i < num; i++) {
+		int year = rand() % 65 + 1950, month = rand()%12 + 1, day = 0;
+
+		switch (month) {
+			case 1: case 3: case 5:
+			case 7: case 8: case 10:
+			case 12:
+				day = rand() % 31 + 1; break;
+			case 2: 
+				day = rand() % 28 + 1; break;
+			case 4: case 6: case 9:
+			case 11:
+				day = rand() % 30 + 1; break;
+		}
+
+		sprintf(birthday, "%04d%02d%02d", year, month, day);
+		birthdayArr[i] = string(birthday);
+
+		cout << birthdayArr[i] << endl;
+	}
+
+	return birthdayArr;
+}
+
+
+
 #endif
 
-
+/*
 // test
 int main() {
-/*
-	string * people = generateRandomMemberName(130);
+*/	/*
+	   string * people = generateRandomMemberName(130);
 
 
 	// Member :: ID
 
 	for (int i = 0; i < 130; i++) {
-		cout << people[i] << endl;
+	cout << people[i] << endl;
 	}
-*/	
+	*/	
 
-	string* phone = generateRandomPhoneNumber(10);
+	// string* phone = generateRandomPhoneNumber(10);
+	
+//	string* arr = generateRandomBirthday(50);
 
 
-//	delete [] people;
-
+/*
 	return 0;
 }
+*/
--- project1/test/list_text/make_list.cpp
+++ project1/test/list_text/make_list.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include "stockmaker.cpp"
+#include "gen_var_member.cpp"
 
 #define NUM_OF_MEMBERS		1300
 #define NUM_OF_STOCKS		1400
@@ -12,63 +14,80 @@
 #define FILENAME_STOCK		"listOfStock.txt"
 #define FILENAME_PURCHASE	"listOfPurchase.txt"
 
-void makeListMember();
-void makeListStock();
-void makeListPurchase();
-void generateRandomString()
+string* makeListMember();
+void makeListPurchase(string* memberIdList,string* stockIdList);
 
 int main () {
 	int listCase = 0;
 
-	printf("");
-	scanf("%d", &listCase);
-	
-	switch (listCase) {
-		case 1: makeListMember()
-	}
+	string* memberIdList = makeListMember()
+		,* stockIdList = makeListStock();
+
+	makeListPurchase(memberIdList, stockIdList);
+
+
+	delete [] memberIdList;
+	delete [] stockIdList;
 
 	return 0;
 }
 
-void makeListMember() {
-	FILE* fpList = NULL;
+string* makeListMember() {
+	FILE* fpMember = fopen("listOfMember.txt", "w");
 
-	if ((fpList = fopen(FILENAME_MEMBER, "w")) == NULL) {
+	if ((fpMember = fopen(FILENAME_MEMBER, "w")) == NULL) {
 		fprintf(stderr, "An error occurs during making a text file - listOfMembers");
 	}
 
 	printf("Make a listofMembers");
 
-	fprintf(fpList, "%d\n", NUM_OF_MEMBERS);
+	fprintf(fpMember, "%d\n", NUM_OF_MEMBERS);
+
+	string* idList		= generateRandomMemberId(NUM_OF_MEMBERS)
+		,** nameList	= generateRandomMemberName(NUM_OF_MEMBERS)
+		,* phoneList	= generateRandomPhoneNumber(NUM_OF_MEMBERS)
+		,* addressList	= generateRandomAddress()
+		,* birthdayList	= generateRandomBirthday(NUM_OF_MEMBERS);
 
 	for (int i = 0; i < NUM_OF_MEMBERS; i++) {
-		int lenID = (rand() % 4) + 5;
+		string fullName = nameList[i][0]+ " " + nameList[i][1],
+			   fullEmail = nameList[i][0] + "@sogang.ac.kr";
 
-		
-
-		fprintf(fpMember, "%s|%s|%s|%s|%8d|%s\n"
-				,							// ID
-				,							// Name
-				,							// Phone Number
-				,							// Address
-				,							// Birthday
-				,							// E-mail
+		fprintf(fpMember, "%s|%s|%s|%s|%s|%s\n"
+				,idList[i].c_str()							// ID
+				,fullName.c_str()							// Name
+				,phoneList[i].c_str()						// Phone Number
+				,addressList[i%135].c_str()						// Address
+				,birthdayList[i].c_str()					// Birthday
+				,fullEmail.c_str()							// E-mail
 				);
 	}
 
-	fclose(fpList);
-}
+	delete [] phoneList;
+	delete [] addressList;
+	delete [] birthdayList;
 
-void makeListStock() {
-	printf("Make a list of Stocks");
+	fclose(fpMember);
 
-
-}
-
-void makeListPurchase() {
-	printf("Make a list of Purchases");
-
-
+	return idList;
 }
 
 
+void makeListPurchase(string* memberIdList,string* stockIdList) {
+	FILE* fp = fopen("listOfPurchase.txt", "w");
+
+	srand(time(NULL));
+	for (int i = 0; i < NUM_OF_PURCHASES; i++) {
+		long long int purchaseId = rand() % 100000000;
+		int quantity = rand() % 100;
+		
+		fprintf(fp, "%016lld|%s|%s|%02d\n",
+				purchaseId
+				, stockIdList[i%NUM_OF_STOCKS].c_str()
+				, memberIdList[i%NUM_OF_MEMBERS].c_str()
+				, quantity);
+	}
+
+	fclose(fp);
+
+}
