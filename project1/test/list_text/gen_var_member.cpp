64f255b25d6669e6fcaadbacc2c7de6d5ce4a9d6
SGsupernova <dlxotmd125@gmail.com>
2016-09-26 17:33:59 +0900

    enlarge the number of names

--- project1/test/list_text/gen_var_member.cpp
+++ project1/test/list_text/gen_var_member.cpp
@@ -1,9 +1,17 @@
 #ifndef __GEN_VAR_MEMBER__
 #define __GEN_VAR_MEMBER__
 
+#define NUM_FIRST_NAME		500
+#define NUM_LAST_NAME		1000
+
 #include <cstdlib>
 #include <cstdio>
 #include <ctime>
+#include <string>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
 
 char*  generateRandomMemberID (void) {
 	srand(time(NULL));
@@ -18,9 +26,53 @@ char*  generateRandomMemberID (void) {
 	return genStr;
 };
 
-char* generateRandomMemberName (void) {
-
+string* generateRandomMemberName (int peopleNum) {
+	ifstream maleFirstFile,
+			 femaleFirstFile,
+			 allLastFile;
 	
+	// file open
+	maleFirstFile.open	("name_list/word_male_first.txt");
+	femaleFirstFile.open("name_list/word_female_first.txt");
+	allLastFile.open	("name_list/word_all_last.txt");
+	
+	// get first and last names.
+	string firstName[2][NUM_FIRST_NAME];
+	string lastName[NUM_LAST_NAME];
+
+	for (int i = 0; i < NUM_FIRST_NAME; i++) {
+		maleFirstFile >> firstName[0][i];
+		femaleFirstFile >> firstName[1][i];
+	}
+	for (int i = 0; i < NUM_LAST_NAME; i++) {
+		allLastFile >> lastName[i];
+	}
+
+	// generate random names.
+	string* peopleName = new string[peopleNum];
+
+	srand(time(NULL));
+	for (int i = 0; i < peopleNum; i++) {
+		int firstNameNumber = rand() % (NUM_FIRST_NAME),
+			lastNameNumber	= rand() % (NUM_LAST_NAME);
+		
+		peopleName[i] = firstName[i%2][firstNameNumber] + " " + lastName[lastNameNumber];
+	}
+
+	return peopleName;
 };
 
 #endif
+
+int main() {
+	string * people = generateRandomMemberName(130);
+
+
+	for (int i = 0; i < 130; i++) {
+		cout << people[i] << endl;
+	}
+	
+	delete [] people;
+
+	return 0;
+}
