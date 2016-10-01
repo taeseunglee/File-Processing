3e35fc237a59992fbec60a7acfe54733ea4da850
SGsupernova <dlxotmd125@gmail.com>
2016-10-02 04:11:51 +0900

    make stockTest

---
+++ src/packingTest/stockTest.cpp
@@ -0,0 +1,40 @@
+#include "../classes/class_stock.h"
+#include "../../include/packing/recfile.h"
+#include <fstream>
+#include <vector>
+
+int main() {
+	ifstream ifs ("../../resources/listOfStock.txt");
+	char filename[ ] = "fileOfStock.dat";
+
+	int n;
+	ifs >> n;
+	ifs.ignore (numeric_limits<streamsize>::max(), '\n');
+
+	DelimFieldBuffer buffer ('|', STK_MAX_BUF);
+	RecordFile <Stock> stockFile (buffer);
+
+	// Write test
+	stockFile.Create (filename, ios::out | ios::trunc);
+	for (int i = 0; i < n; i++) {
+		Stock m;
+		ifs >> m;
+		
+		int recAddr;
+		if ((recAddr = stockFile.Write(m)) == -1) { cout << "Write Error!" << endl; }
+		else { cout << "Write at " << recAddr << endl; }
+	}
+	stockFile.Close ();
+
+
+	// Read Test
+	stockFile.Open (filename, ios::in);
+	for (int i = 0; i < n; i++) {
+		Stock m;
+		stockFile.Read(m);
+		cout << m;
+	}
+	stockFile.Close();
+
+	return 0;
+}
