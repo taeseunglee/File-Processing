0935994f0902025ef56c01efbcfa283b408aeea8
SGsupernova <dlxotmd125@gmail.com>
2016-10-02 20:13:37 +0900

    main function

---
+++ src/onlineShoppingSystem.cpp
@@ -0,0 +1,15 @@
+#include "./showClass/showMember.cpp"
+#include "./showClass/showPurchase.cpp"
+#include "./showClass/showStock.cpp"
+
+#include "./packingTest/memberTest.cpp"
+#include "./packingTest/purchaseTest.cpp"
+#include "./packingTest/stockTest.cpp"
+
+
+int main() {
+	showMember();
+	Member a;
+
+	return 0;
+}
