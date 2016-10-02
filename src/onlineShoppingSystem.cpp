2ea02bf2fd2031ecaf9416db7e5cad982ce0650f
SGsupernova <dlxotmd125@gmail.com>
2016-10-02 20:45:26 +0900

    print a menu

--- src/onlineShoppingSystem.cpp
+++ src/onlineShoppingSystem.cpp
@@ -6,10 +6,34 @@
 #include "./packingTest/purchaseTest.cpp"
 #include "./packingTest/stockTest.cpp"
 
+bool selectMenu ();
 
 int main() {
-	showMember();
-	Member a;
+	while(selectMenu()) {}
 
 	return 0;
+}
+
+bool selectMenu () {
+	int menuNum;
+
+	cout << "=================================================" << endl;
+	cout << "\t\tSelect the memu." << endl;
+	cout << "1: showMember" << endl;
+	cout << "2: showStock" << endl;
+	cout << "3: showPurchase" << endl;
+	cout << "4: MemberTest" << endl;
+	cout << "5: StockTest" << endl;
+	cout << "6: Purchasetest" << endl;
+	cout << "7: TicketPurchaseSystem" << endl;
+	cout << "8: EXIT" << endl << endl;
+	cout << "Please enter the number of the menu." << endl;
+	cout << "=================================================" << endl << ">> ";
+
+	cin >> menuNum;
+
+	
+
+
+	return false;
 }
