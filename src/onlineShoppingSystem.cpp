bf5c8215b3816e653fbd7874d57221e43fb99825
SGsupernova <dlxotmd125@gmail.com>
2016-11-09 23:06:19 +0900

    add admin mode and general user mode

--- src/onlineShoppingSystem.cpp
+++ src/onlineShoppingSystem.cpp
@@ -17,7 +17,9 @@
 #include "./record/recordModify.h"
 
 bool selectMenu (Environment &env);
-bool ticketPurchaseSystem (Environment &env);
+void loginPurchaseSystem (Environment env);
+bool adminModePurchaseSystem (Environment &env);
+bool generalUserModePurchaseSystem (Environment &env, string id);
 
 
 int main() {
@@ -61,21 +63,51 @@ bool selectMenu (Environment &env) {
 		case 5: stockTest(env); break;
 		case 6: purchaseTest(env); break;
 
-		case 7: while(ticketPurchaseSystem(env)); break;
+		case 7: loginPurchaseSystem(env); break;
 		case 8: default: return result = false;
 	}
 
 	return result;
 }
 
-bool ticketPurchaseSystem (Environment &env) {
-	cout << "=================================================" << endl << ">> ";
-	cout << "\t\tTicketPurchaseSystem" << endl;
+// return Member's level. if return -1 then loggin failed.
+void loginPurchaseSystem (Environment env) {
+	string id, password;
+	cout << "=================================================" << endl;
+	cout << "\tTicketPurchaseSystem - Login Window -" << endl;
+	cout << "=================================================" << endl;
+	cout << "Enter ID and Password" << endl;
+	cout << "ID : "; cin >> id;
+	cout << "PW : "; cin >> password;
+	
+	// Admin or TestUser only can pass login.
+	// Start Admin Mode
+	int level = 0;
+	if (!id.compare("admin")) {
+		if (!password.compare("adminpass")) level = 1;
+		else level = -1;
+	}
+	else if (!id.compare("TestUser")){
+		if (!password.compare("T1234")) level = 9;
+		else level = -1;
+	}
+	else level = -1;
+			
+	switch (level) {
+		case -1: cout << "Login Failed" << endl; break;
+		case 1: while(adminModePurchaseSystem(env)); break;
+		case 9: while(generalUserModePurchaseSystem(env, id));break;
+	}
+}
+
+bool adminModePurchaseSystem (Environment &env) {
+	cout << "=================================================" << endl;
+	cout << "\tTicketPurchaseSystem - Admin Mode -" << endl;
 	cout << "1: Search record" << endl;
 	cout << "2: Insert record" << endl;
 	cout << "3: Delete record" << endl;
 	cout << "4: Modify record" << endl;
-	cout << "5: Close TicketPurchaseSystem" << endl << endl;
+	cout << "5: Close AdminMode" << endl << endl;
 	cout << "Please enter the number of the menu." << endl;
 	cout << "=================================================" << endl << ">> ";
 
@@ -89,6 +121,91 @@ bool ticketPurchaseSystem (Environment &env) {
 		case 3: recordDeleteMain(env); break;
 		case 4: recordModifyMain(env); break;
 		case 5: default: return result = false;
+	}
+
+	return result;
+}
+
+
+bool generalUserModePurchaseSystem (Environment &env, string memberId) {
+	cout << "=================================================" << endl;
+	cout << "\t\tTicketPurchaseSystem - General User Mode -" << endl;
+	cout << "1: Modify Your record" << endl;
+	cout << "2: Unregister Your account" << endl;
+	cout << "3: Search Stock List" << endl;
+	cout << "4: Search your Purchase List" << endl;
+	cout << "5: Insert record in Purchase record" << endl;
+	cout << "6: Modify Your Purchase record" << endl;
+	cout << "7: Delete Your Purchase record" << endl;
+	cout << "8: Close General User Mode" << endl << endl;
+	cout << "Please enter the number of the menu." << endl;
+	cout << "=================================================" << endl << ">> ";
+
+	int menuNum;
+	cin >> menuNum;
+
+	bool result = true;
+	switch (menuNum) {
+		case 1: recordModifyMember(env, memberId); break;
+
+		case 2: {
+					Member mTemp;
+					recordDeletePurchaseMID(env, memberId);
+					recordDeleteDat(env.memberData[memberId], "../built/fileOfMember.dat", mTemp);
+					env.memberData.erase(memberId);
+				} return result = false;
+
+		case 3: {
+					bool findFlag = false;
+					DelimFieldBuffer buffer ('|', MEM_MAX_BUF);
+					char *filename = new char [MAX_FILENAME];
+					string id;
+
+					cout << "Enter Stock Id that you search" << endl << ">> ";
+					cin >> id;
+
+					FINDINDATA(env.stockData, id, findFlag);
+					if (findFlag) {
+						RecordFile <Stock> stockFile (buffer);
+						Stock s;
+
+						strlcpy(filename, "../built/fileOfStock.dat", MAX_FILENAME - 1);
+						stockFile.Open(filename, ios::in);
+						stockFile.Read(s, env.stockData[id]);
+						cout << s << endl;
+
+						stockFile.Close();
+						delete [] filename;
+					}
+				} break;
+
+		case 4: {
+					findFromEnv(env.purchaseList, memberId, 1);
+				} break;
+
+		case 5: {
+					Purchase pt;
+					string filename = "../built/fileOfPurchase.dat";
+					recordInsert(env, env.purchaseData, filename, pt);
+				} break;
+		case 6: {
+					recordModifyPurchase(env);
+				} break;
+		case 7: {
+					string id;
+					cout << "Enter id that you want to delete." << endl << ">> ";
+					cin >> id;
+					vector<Purchase> pList = env.purchaseList;
+					std::vector<Purchase>::iterator it = pList.begin();
+					for (; it != pList.end(); ++it) {
+						if ((*it).getId() == id)
+							break;
+					}
+					if (it != pList.end())
+						recordDeletePurchasePID(env, it);
+				} break;
+
+		case 8: default: return result = false;
 	}
 
 	return result;
